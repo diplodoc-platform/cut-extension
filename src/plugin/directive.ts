b47215f826693ad98ce9c3f6850e0702be3df543
d3m1d0v <d3m1d0v@yandex-team.ru>
2024-11-07 18:51:26 +0300

    feat(plugin): register cut directive with config

--- src/plugin/directive.ts
+++ src/plugin/directive.ts
@@ -1,51 +1,42 @@
 import type MarkdownIt from 'markdown-it';
 
-import {
-    createBlockInlineToken,
-    directive,
-    registerBlockDirective,
-    tokenizeBlockContent,
-} from '@diplodoc/directive';
+import {directiveParser, registerBlockDirective} from '@diplodoc/directive';
 
 import {ClassNames, ENV_FLAG_NAME, TokenType} from './const';
 
 export const cutDirective: MarkdownIt.PluginSimple = (md) => {
-    md.use(directive());
+    md.use(directiveParser());
 
-    registerBlockDirective(md, 'cut', (state, params) => {
-        if (!params.content) {
-            return false;
-        }
+    registerBlockDirective(md, {
+        name: 'cut',
+        type: 'container',
+        match(_params, state) {
+            state.env ??= {};
+            state.env[ENV_FLAG_NAME] = true;
 
-        state.env ??= {};
-        state.env[ENV_FLAG_NAME] = true;
-
-        let token = state.push(TokenType.CutOpen, 'details', 1);
-        token.block = true;
-        token.attrSet('class', ClassNames.Cut);
-        token.map = [params.startLine, params.endLine];
-        token.markup = ':::';
-
-        token = state.push(TokenType.TitleOpen, 'summary', 1);
-        token.attrSet('class', ClassNames.Title);
-
-        if (params.inlineContent) {
-            token = createBlockInlineToken(state, params);
-        }
-
-        token = state.push(TokenType.TitleClose, 'summary', -1);
-
-        token = state.push(TokenType.ContentOpen, 'div', 1);
-        token.attrSet('class', ClassNames.Content);
-        token.map = [params.startLine + 1, params.endLine - 1];
-
-        tokenizeBlockContent(state, params.content, 'yfm-cut');
-
-        token = state.push(TokenType.ContentClose, 'div', -1);
-
-        token = state.push(TokenType.CutClose, 'details', -1);
-        token.block = true;
-
-        return true;
+            return true;
+        },
+        container: {
+            tag: 'details',
+            token: TokenType.Cut,
+            attrs: {
+                class: ClassNames.Cut,
+            },
+        },
+        inlineContent: {
+            required: false,
+            tag: 'summary',
+            token: TokenType.Title,
+            attrs: {
+                class: ClassNames.Title,
+            },
+        },
+        content: {
+            tag: 'div',
+            token: TokenType.Content,
+            attrs: {
+                class: ClassNames.Content,
+            },
+        },
     });
 };
