1cfb113913cd19ab9e14b078be9caec3cf863e1d
3y3 <3y3@ya.ru>
2025-08-27 11:52:57 +0300

    feat!: Remove useless click handler
    `datails` tag manages his state by itself.
    We don't need to handle click on `details`

--- src/runtime/controller.ts
+++ src/runtime/controller.ts
@@ -1,18 +1,6 @@
-import {ClassName, Selector} from './const';
-import {getEventTarget, isCustom} from './utils';
+import {Selector} from './const';
 
 export class YfmCutController {
-    private readonly __doc: Document;
-
-    constructor(doc: Document) {
-        this.__doc = doc;
-        this.__doc.addEventListener('click', this._onDocClick);
-    }
-
-    destroy() {
-        this.__doc.removeEventListener('click', this._onDocClick);
-    }
-
     focusActiveCut() {
         const cutId = window.location.hash.slice(1);
         const cutNode = document.getElementById(cutId) as HTMLDetailsElement | null;
@@ -35,39 +23,6 @@ export class YfmCutController {
 
         setTimeout(() => {
             cutNode.classList.remove('cut-highlight');
-        }, 1_000);
-    }
-
-    private _onDocClick = (event: MouseEvent) => {
-        if (isCustom(event)) {
-            return;
-        }
-
-        const title = this._findTitleInPath(event);
-        if (title) {
-            this._toggleCut(title);
-        }
-    };
-
-    private _findTitleInPath(event: MouseEvent): HTMLElement | undefined {
-        const target = getEventTarget(event);
-        if (this._matchTitle(target)) {
-            return target as HTMLElement;
-        }
-
-        const path = event.composedPath?.();
-        return path?.find(this._matchTitle) as HTMLElement | undefined;
-    }
-
-    private _matchTitle = (target: EventTarget | null) => {
-        if (target instanceof HTMLElement) {
-            return target?.matches?.(Selector.TITLE);
-        }
-        return false;
-    };
-
-    private _toggleCut(element: HTMLElement) {
-        const cutNode = element.parentElement;
-        cutNode?.classList.toggle(ClassName.OPEN);
+        }, 1000);
     }
 }
